9735aa85c9221761cbc0d914fd0a9a5a15d6d713
Gabriel Santos De Lima Barreto <gabriel.devloper@gmail.com>
2019-06-14 19:45:50 -0300

    aula 15

---
+++ Aula-15/ambiente.js
@@ -0,0 +1,13 @@
+let numero = [5,8,2,9,3];
+numero.sort();//Função ordena o vetor
+for(let i = 0; i<numero.length; i++){
+    console.log(`${numero[i]}`);
+}
+
+console.log("\n");
+
+for(let pos in numero){
+    console.log(numero[pos]);
+
+}
+
